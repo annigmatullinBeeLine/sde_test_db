48d403120f97ae2ce93879957195435678d1b228
annigmatullinBeeLine <139735163+annigmatullinBeeLine@users.noreply.github.com>
2023-07-18 17:01:02 +0300

    Add files via upload

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+docker run --name postgres \
+-p 5432:5432 \
+-e POSTGRES_DB="demo" \
+-e POSTGRES_USER="test_sde" \
+-e POSTGRES_PASSWORD="@sde_password012" \
+-d postgres
+ docker cp $(pwd)/../sql postgres://var/lib/postgresql/data
+ sleep 5
+winpty docker exec postgres psql -U test_sde -d demo -f //var/lib/postgresql/data/sql/init_db/demo.sql
+
+ sleep 200
+
+
+
